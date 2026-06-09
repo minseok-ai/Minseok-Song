6b4174d62113eeee92dffba9b5979d587342ee2e
minseok-ai <minseoksong.ai@gmail.com>
2026-06-09 13:38:49 +0900

    v0.05.1 Update: Bug Fixes

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,14 +1,11 @@
 import GitHub from '@auth/core/providers/github';
 import { defineConfig } from 'auth-astro';
-import { loadEnv } from 'vite';
-
-const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
 export default defineConfig({
   providers: [
     GitHub({
-      clientId: env.GITHUB_CLIENT_ID || process.env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET || process.env.GITHUB_CLIENT_SECRET,
+      clientId: import.meta.env.GITHUB_CLIENT_ID ?? process.env.GITHUB_CLIENT_ID,
+      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET ?? process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
 });
